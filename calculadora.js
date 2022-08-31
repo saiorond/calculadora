4259cb6035262ed65b64c141c64be50577625305
Débora <d.saioron@hotmail.com>
2022-08-30 21:50:10 -0300

    selecionando botoes no js

---
+++ calculadora.js
@@ -0,0 +1,6 @@
+const botoesNumeros = document.querySelectorAll("[data-numeros]");
+const botoesOperadores = document.querySelectorAll("[data-operadores]");
+const botaoIgual = document.querySelector("[data-igual]");
+const botaoDell = document.querySelector("[data-del]");
+const operacaoAnterior = document.querySelector("[data-operacao-anterior]");
+const operacaoAtual = document.querySelector("[data-operacao-atual]");
