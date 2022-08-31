5964871ff0245fab071e68f5f4322531be3fe229
Débora <d.saioron@hotmail.com>
2022-08-31 20:33:34 -0300

    criando classe

--- calculadora.js
+++ calculadora.js
@@ -2,5 +2,28 @@ const botoesNumeros = document.querySelectorAll("[data-numeros]");
 const botoesOperadores = document.querySelectorAll("[data-operadores]");
 const botaoIgual = document.querySelector("[data-igual]");
 const botaoDell = document.querySelector("[data-del]");
-const operacaoAnterior = document.querySelector("[data-operacao-anterior]");
-const operacaoAtual = document.querySelector("[data-operacao-atual]");
+const botaoLimpaTudo = document.querySelector("[data-limpar-tudo]");
+const operacaoAnteriorTexto = document.querySelector("[data-operacao-anterior]");
+const operacaoAtualTexto = document.querySelector("[data-operacao-atual]");
+
+class Calculadora {
+    constructor(operacaoAnteriorTexto, operacaoAtualTexto) {
+        this.operacaoAnteriorTexto = operacaoAnteriorTexto;
+        this.operacaoAtualTexto = operacaoAtualTexto;
+    }
+
+    clear () {
+        this.operacaoAnteriorTexto = "";
+        this.operacaoAtualTexto = "";
+        this.operacao = undefined;
+    }
+
+    atualizaDisplay() {
+        this.operacaoAnteriorTexto.innerText = this.operacaoAnterior;
+        this.operacaoAtualTexto.innerText = this.operacaoAtual;
+    }
+}
+
+const calculadora = new Calculadora(
+    operacaoAnteriorTexto, operacaoAtualTexto
+);
