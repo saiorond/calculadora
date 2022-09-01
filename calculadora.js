2cc873cd44c1344d9467f00ab694f4d19e8e0bd6
Débora <d.saioron@hotmail.com>
2022-08-31 21:00:51 -0300

    criando funções para mostrar o número no display

--- calculadora.js
+++ calculadora.js
@@ -10,20 +10,38 @@ class Calculadora {
     constructor(operacaoAnteriorTexto, operacaoAtualTexto) {
         this.operacaoAnteriorTexto = operacaoAnteriorTexto;
         this.operacaoAtualTexto = operacaoAtualTexto;
+        this.clear();
+    }
+
+    mostraNumero(number) {
+        this.operacaoAtual = `${this.operacaoAtual}${number.toString()}`;
     }
 
     clear () {
-        this.operacaoAnteriorTexto = "";
-        this.operacaoAtualTexto = "";
+        this.operacaoAnterior = "";
+        this.operacaoAtual = "";
         this.operacao = undefined;
     }
-
+ 
     atualizaDisplay() {
         this.operacaoAnteriorTexto.innerText = this.operacaoAnterior;
         this.operacaoAtualTexto.innerText = this.operacaoAtual;
     }
+
 }
 
 const calculadora = new Calculadora(
     operacaoAnteriorTexto, operacaoAtualTexto
 );
+
+for (const botaoNumero of botoesNumeros) {
+    botaoNumero.addEventListener("click", () => {
+        calculadora.mostraNumero(botaoNumero.innerText);
+        calculadora.atualizaDisplay();
+    })
+}
+
+botaoLimpaTudo.addEventListener("click", () => {
+    calculadora.clear();
+    calculadora.atualizaDisplay();
+})
